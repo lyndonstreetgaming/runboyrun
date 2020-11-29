24da9e375a3c7b7195b92d0d8289cea25b770989
lyndonstreetgaming <54155350+lyndonstreetgaming@users.noreply.github.com>
2020-11-29 15:49:03 -0400

    Updating PlayerController.cs and add new animations.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Start() Variables
     private Rigidbody2D RigidBody;
-
-    private Animator PlayerAnimation;
-
-    private enum State { Idle, Running, Jumping, Falling }
-
-    private State state = State.Idle;
 
     private Collider2D Colliders;
 
+    private Animator PlayerAnimation;
+    
+    //FSM
+    private enum State { Idle, Running, Jumping, Falling }
+
+    private State state = State.Idle;
+   
+    //All these are inspector Variables
     [SerializeField]
 
     private LayerMask Ground;
+
+    [SerializeField]
+
+    //Yuh wah Speed? 
+    private float Speed = 5f;
+
+    [SerializeField]
+
+    //Jump boy, JUMP!
+    private float JumpingForce = 18f;
 
     private void Start()
     {
@@ -29,42 +42,49 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        Directions();
+
+        AnimationState();
+
+        //Set animation based on Enumerator state and thing.
+        PlayerAnimation.SetInteger("state", (int)state);
+    }
+
+    private void Directions()
+    {
         float HorizontalDirection = Input.GetAxis("Horizontal");
+
+        //This is to make it move left.
 
         if (HorizontalDirection < 0)
         {
-            RigidBody.velocity = new Vector2(-5, RigidBody.velocity.y);
+            RigidBody.velocity = new Vector2(-Speed, RigidBody.velocity.y);
 
             transform.localScale = new Vector2(-1, 1);
 
         }
 
-       else if (HorizontalDirection > 0)
+        //This is to make it move the opposite of left which is right. 
+
+        else if (HorizontalDirection > 0)
         {
-            RigidBody.velocity = new Vector2(5, RigidBody.velocity.y);
+            RigidBody.velocity = new Vector2(Speed, RigidBody.velocity.y);
 
             transform.localScale = new Vector2(1, 1);
 
         }
 
-        else
-        {
-
-        }
+        //This is to make it jump.
 
         if (Input.GetButtonDown("Jump") && Colliders.IsTouchingLayers(Ground))
         {
-            RigidBody.velocity = new Vector2(RigidBody.velocity.x, 18f);
+            RigidBody.velocity = new Vector2(RigidBody.velocity.x, JumpingForce);
 
             state = State.Jumping;
         }
-
-        VelocityState();
-
-        PlayerAnimation.SetInteger("state", (int)state);
     }
 
-    private void VelocityState()
+    private void AnimationState()
     {
         if (state == State.Jumping)
         {
