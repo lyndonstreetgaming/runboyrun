85b66254bc438a06feffd38c07b4fb85ffde4135
lyndonstreetgaming <54155350+lyndonstreetgaming@users.noreply.github.com>
2020-12-08 03:48:09 -0400

    Seeker Bot AI

--- Assets/Scripts/Seeker_Bot.cs
+++ Assets/Scripts/Seeker_Bot.cs
@@ -12,5 +12,110 @@ public class Seeker_Bot : MonoBehaviour
 
     private float RightCapture;
 
-    private bool IsFacing = true;
+    [SerializeField]
+
+    private float JumpingLength = 10f;
+
+    [SerializeField]
+
+    private float JumpingHeight = 15f;
+
+    [SerializeField]
+
+    private LayerMask Ground;
+
+    private Collider2D Colliders;
+
+    private Rigidbody2D RigidBody;
+
+    private Animator SeekerBotAnimation;
+
+    private bool FacingLeft = true;
+
+    private void Start()
+    {
+        Colliders = GetComponent<Collider2D>();
+
+        RigidBody = GetComponent<Rigidbody2D>();
+
+        SeekerBotAnimation = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Transition from Jump to Fall.
+        if (SeekerBotAnimation.GetBool("Jumping"))
+        {
+            if (RigidBody.velocity.y < .1)
+            {
+                SeekerBotAnimation.SetBool("Falling", true);
+
+                SeekerBotAnimation.SetBool("Jumping", false);
+            }
+        }
+
+        //Transition from Fall to Idle
+        if (Colliders.IsTouchingLayers(Ground) && SeekerBotAnimation.GetBool("Falling"))
+        {
+            SeekerBotAnimation.SetBool("Falling", false);
+
+
+        }
+    }
+
+    private void Movement()
+    {
+        if (FacingLeft)
+        {
+            //Test to see if we are beyond the Left Capture.
+            if (transform.position.x > LeftCapture)
+            {
+                //Make sure sprite is facing the right location, and if it is not, then face the right direction.
+                if (transform.localScale.x != 1)
+                {
+                    transform.localScale = new Vector3(1, 1);
+                }
+
+                //Test to see if Seeker Bot is on the ground, if so jump.
+                if (Colliders.IsTouchingLayers(Ground))
+                {
+                    //Jump, duh!
+                    RigidBody.velocity = new Vector2(-JumpingLength, JumpingHeight);
+
+                    SeekerBotAnimation.SetBool("Jumping", true);
+                }
+            }
+
+            else
+            {
+                FacingLeft = false;
+            }
+        }
+
+        else
+        {
+            if (transform.position.x < RightCapture)
+            {
+                //Make sure sprite is facing the right location, and if it is not, then face the right direction.
+                if (transform.localScale.x != 1)
+                {
+                    transform.localScale = new Vector3(1, 1);
+                }
+
+                //Test to see if Seeker Bot is on the ground, if so jump.
+                if (Colliders.IsTouchingLayers(Ground))
+                {
+                    //Jump, duh!
+                    RigidBody.velocity = new Vector2(-JumpingLength, JumpingHeight);
+
+                    SeekerBotAnimation.SetBool("Jumping", true);
+                }
+            }
+
+            else
+            {
+                FacingLeft = true;
+            }
+        }
+    }
 }
