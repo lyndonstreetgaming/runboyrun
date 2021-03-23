6e90d43e7fe980721b827b2f7ef799ea4a3ad578
lyndonstreetgaming <54155350+lyndonstreetgaming@users.noreply.github.com>
2021-03-23 01:59:45 -0400

    Designing the Main Menu

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Menu : MonoBehaviour
+{
+    public GameObject MainMenu;
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            MainMenu.SetActive(MainMenu.activeSelf);
+        }
+    }
+}
