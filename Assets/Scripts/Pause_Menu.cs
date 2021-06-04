eb038aac07722b73e496f651d4cdcdd94ec8fbb4
lyndonstreetgaming <54155350+lyndonstreetgaming@users.noreply.github.com>
2021-06-04 15:57:38 -0400

    Pause Menu

---
+++ Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public GameObject Pause_UI;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        Pause_UI.SetActive(!Pause_UI.activeSelf);
+
+        if (Pause_UI.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
